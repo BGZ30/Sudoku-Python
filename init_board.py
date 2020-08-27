0e38193036d0441faf3f62de1a494ab4fc2c2ddd
bgz30 <44124521+BGZ30@users.noreply.github.com>
2020-08-27 20:09:46 +0200

    build a random board

--- init_board.py
+++ init_board.py
@@ -2,8 +2,19 @@ import random
 import linecache
     
 filename = "5.txt"
-m = random.randint(0,10000)
+m = random.randint(0,10001)
 
 # retrieve specific line
-model = linecache.getline(filename, m)
-print (model)
+model = linecache.getline(filename, 2)
+
+# reshape to 9x9 matrix
+sudoko_board = [[], [], [],[], [], [],[], [], []]
+               
+for r in range(9):
+    for c in range(9):
+        sudoko_board[r].append(model[c + 9*r])
+
+'''print()
+print(model)
+print(sudoko_board)
+'''
