b34ca47cdf9ebb4ac6722af7df5e8fcf213156c3
bgz30 <44124521+BGZ30@users.noreply.github.com>
2020-08-27 14:30:28 +0200

    find an empty cell

---
+++ find_cell.py
@@ -0,0 +1,25 @@
+
+""" find an empty cell in the sudoku board"""
+def find_cell(board):
+    for r in range(9):
+        for c in range(9):            
+            if board[r][c] == 0:
+                return (r, c)  # the cell index
+    
+    return None # no empty cells found
+            
+
+board = [
+    [7, 8, 0, 4, 0, 0, 1, 2, 0],
+    [6, 0, 0, 0, 7, 5, 0, 0, 9],
+    [0, 0, 0, 6, 0, 1, 0, 7, 8],
+    [0, 0, 7, 0, 4, 0, 2, 6, 0],
+    [0, 0, 1, 0, 5, 0, 9, 3, 0],
+    [9, 0, 4, 0, 6, 0, 0, 0, 5],
+    [0, 7, 0, 3, 0, 0, 0, 1, 2],
+    [1, 2, 0, 0, 0, 7, 4, 0, 0],
+    [0, 4, 9, 2, 0, 6, 0, 0, 7]
+]
+
+
+print(find_cell(board))
