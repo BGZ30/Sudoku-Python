5762a0f2f80c1fcc20718e6cdd0e2ee86bdf9017
bgz30 <44124521+BGZ30@users.noreply.github.com>
2020-08-27 20:18:12 +0200

    fixt type to int

--- init_board.py
+++ init_board.py
@@ -1,18 +1,21 @@
 import random
 import linecache
+
+def build_board():    
+    filename = "5.txt"
+    m = random.randint(0,10001)
+
+    # retrieve specific line
+    model = linecache.getline(filename, 2)
+
+    # reshape to 9x9 matrix
+    sudoko_board = [[], [], [],[], [], [],[], [], []]
+                   
+    for r in range(9):
+        for c in range(9):
+            sudoko_board[r].append(int(model[c + 9*r]))
     
-filename = "5.txt"
-m = random.randint(0,10001)
-
-# retrieve specific line
-model = linecache.getline(filename, 2)
-
-# reshape to 9x9 matrix
-sudoko_board = [[], [], [],[], [], [],[], [], []]
-               
-for r in range(9):
-    for c in range(9):
-        sudoko_board[r].append(model[c + 9*r])
+    return sudoko_board
 
 '''print()
 print(model)
